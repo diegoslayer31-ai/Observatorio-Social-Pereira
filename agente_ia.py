c186d904195e6ee98ef507642199f2939f478dca
diegoslayer31-ai <diegoslayer31@gmail.com>
2026-06-02 15:45:16 -0500

    Add files via upload

--- agente_ia.py
+++ agente_ia.py
@@ -45,8 +45,6 @@ df = pd.read_sql("SELECT * FROM habitante_de_calle", engine)
 # CARGAR DATOS
 # =========================
 df = pd.read_sql("SELECT * FROM habitante_de_calle", engine)
-st.write(df.columns.tolist())
-st.stop()
 df = df.drop_duplicates()
 # 🔥 LIMPIEZA DE COLUMNAS (OBLIGATORIO)
 df.columns = (
